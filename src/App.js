5cac0fb4bd30939a37925ab55641458d757c77fe
Ryan Rouleau <ryan@ryanrouleau.com>
2019-01-14 16:25:04 -0700

    Header & init Projectgs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Projects from './components/Projects';
+
+const Header = styled.div`
+  width: 100%;
+  height: 80px;
+  background: #fff;
+  box-shadow: 0 0 30px 0 rgba(0,0,0,.08), 0 3px 2px 0 rgba(0,0,0,.05);
+`;
+
+const Title = styled.div`
+  font-family: 'Raleway';
+  font-weight: 800;
+  font-size: 44px;
+  width: 60%;
+  margin: auto;
+  padding-top: 16px;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header>
+          <Title>Projects</Title>
+        </Header>
+        <Projects data={{}} />
       </div>
     );
   }
